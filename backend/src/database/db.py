1c23cf91e5b85989bfb8ac899aaa6c79023fcf75
Germán Rojas <garcejr@gmail.com>
2025-02-17 20:49:37 -0500

    Implement connection pooling for PostgreSQL database

--- backend/src/database/db.py
+++ backend/src/database/db.py
@@ -1,16 +1,24 @@
 import psycopg2
-from psycopg2 import DatabaseError
+from psycopg2 import DatabaseError, pool
 from decouple import config
+ 
+# Crear un pool de conexiones
+connection_pool = pool.SimpleConnectionPool(
+    1, 20,
+    database=config('PGSQL_DATABASE'),
+    user=config('PGSQL_USER'),
+    password=config('PGSQL_PASSWORD'),
+    host=config('PGSQL_HOST'),
+    port=config('PGSQL_PORT')
+)
 
 def get_connection():
     try:
-        conn = psycopg2.connect(
-            database=config('PGSQL_DATABASE'),
-            user=config('PGSQL_USER'),
-            password=config('PGSQL_PASSWORD'),
-            host=config('PGSQL_HOST')
-        )
-        return conn
-    except DatabaseError as e:
-        print(e)
+        return connection_pool.getconn()
+    except Exception as e:
+        print(f"Error obtenido conexión: {e}")
         return None
+    
+def release_connection(conn):
+    if conn:
+        connection_pool.putconn(conn)
