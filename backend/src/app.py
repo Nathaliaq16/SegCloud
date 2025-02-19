b53d429bd3c3421b6ad38bd5f64651edb440f4ef
Germán Rojas <garcejr@gmail.com>
2025-02-18 21:53:37 -0500

    Refactor app.py to initialize Flask-Limiter and register blueprints for routes

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,9 +2,10 @@ import os
 from flask import Flask
 from flask_cors import CORS
 from flask_uploads import UploadSet, configure_uploads
-from routes import Carro, Review, Usuario
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+
+app = Flask(__name__)
 
 # Configuración de Flask-Limiter
 limiter = Limiter(
@@ -13,7 +14,6 @@ limiter = Limiter(
     default_limits=["5 per minute"]
 )
 
-app = Flask(__name__)
 limiter.init_app(app) # Inicializar Flask-Limiter
 
 # Configuración de Flask-Uploads
@@ -29,19 +29,21 @@ CORS(app, resources={"*": {"origins": "*"}})
 def page_not_found(error):
     return "Page not found", 404
 
+from routes import Carro, Review, Usuario
+
+# Blueprints
+app.register_blueprint(Carro.carro_bp, url_prefix='/api/carros')
+app.register_blueprint(Review.review_bp, url_prefix='/api/reviews')
+app.register_blueprint(Usuario.usuario_bp, url_prefix='/api/usuarios')
+
+# Error handling
+app.register_error_handler(404, page_not_found)
+
 # Cargar configuración desde variables de entorno
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'supersupersecreto')
 app.config['DEBUG'] = os.getenv('DEBUG', 'True').lower() == 'true'
 
 if __name__ == '__main__':
-    # Blueprints
-    app.register_blueprint(Carro.carro_bp, url_prefix='/api/carros')
-    app.register_blueprint(Review.review_bp, url_prefix='/api/reviews')
-    app.register_blueprint(Usuario.usuario_bp, url_prefix='/api/usuarios')
-
-    # Error handling
-    app.register_error_handler(404, page_not_found)
-
     # Definir el puerto desde variables de entorno o usar 5000 por defecto
     PORT = int(os.getenv('PORT', 5000))
 
