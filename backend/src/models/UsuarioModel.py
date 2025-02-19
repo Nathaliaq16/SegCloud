4b028cc90c7fc2f2b72fe71896340681e392b333
Germán Rojas <garcejr@gmail.com>
2025-02-18 19:49:59 -0500

    Hash password before storing in UsuarioModel

--- backend/src/models/UsuarioModel.py
+++ backend/src/models/UsuarioModel.py
@@ -1,5 +1,6 @@
 from database.db import get_connection, release_connection
 from .entities.Usuario import Usuario
+from utils.security import hash_password
 
 
 class UsuarioModel:
@@ -53,14 +54,14 @@ class UsuarioModel:
             connection = get_connection()
 
             with connection.cursor() as cursor:
+                password_hashed = hash_password(usuario.password_hash) # Ciframos la contraseña
                 cursor.execute("""INSERT INTO public.usuarios (username, email, password_hash, first_name, last_name, phone_number, address, city, country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin)
                                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                               (usuario.username, usuario.email, usuario.password_hash, usuario.first_name, usuario.last_name, usuario.phone_number, usuario.address, usuario.city, usuario.country, usuario.birthdate, usuario.is_seller, usuario.seller_rating, usuario.cars_sold, usuario.registration_date, usuario.last_login, usuario.is_active, usuario.is_admin))
-                affected_rows = cursor.rowcount
+                               (usuario.username, usuario.email, password_hashed, usuario.first_name, usuario.last_name, usuario.phone_number, usuario.address, usuario.city, usuario.country, usuario.birthdate, usuario.is_seller, usuario.seller_rating, usuario.cars_sold, usuario.registration_date, usuario.last_login, usuario.is_active, usuario.is_admin))
                 connection.commit()
 
             connection.close()
-            return affected_rows
+            return {"message": "Usuario agregado exitosamente"}
         except Exception as e:
             print(f"Error en add_usuario: {e}")
             return {"error": "Error al agregar el usuario"}
