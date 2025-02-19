8c98b2d27046c6730e5cb34ebc23c6d8f87f1574
Germán Rojas <garcejr@gmail.com>
2025-02-18 22:34:02 -0500

    Refactor Usuario routes to simplify code, enhance error handling, and improve response consistency

--- backend/src/models/UsuarioModel.py
+++ backend/src/models/UsuarioModel.py
@@ -9,59 +9,55 @@ class UsuarioModel:
 
     @classmethod
     def get_usuarios(cls):
+        """ Obtiene todos los usuarios de la base de datos. """
         try:
             connection = get_connection()
             usuarios = []
 
             with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM public.usuarios ORDER BY id ASC")
+                cursor.execute("SELECT id, username, email, first_name, last_name, phone_number, address, city, country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin FROM public.usuarios ORDER BY id ASC")
                 resultset = cursor.fetchall()
 
                 for usuario in resultset:
-                    user = Usuario(*usuario)
-                    usuarios.append(user.to_JSON())
+                    usuarios.append(Usuario(*usuario).to_JSON())
 
-            connection.close()
             return usuarios
         except Exception as e:
             print(f"Error en get_usuarios: {e}")
-            return {"error": "Error al obtener los usuarios"}
+            return {"error": "Error al obtener los usuarios"}, 500
         finally:
             release_connection(connection)
 
     @classmethod
     def get_usuario(cls, id):
+        """ Obtiene un usuario por su ID. """
+        if not isinstance(id, int) or id <= 0:
+            return {"error": "ID inválido"}, 400
+
         try:
             connection = get_connection()
-
             with connection.cursor() as cursor:
-                cursor.execute("SELECT * FROM public.usuarios WHERE id=%s", (id,))
+                cursor.execute("SELECT id, username, email, first_name, last_name, phone_number, address, city, country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin FROM public.usuarios WHERE id=%s", (id,))
                 usuario = cursor.fetchone()
 
-                user = None
-                if usuario is not None:
-                    user = Usuario(*usuario).to_JSON()
-
-            connection.close()
-            return user
+                if usuario:
+                    return Usuario(*usuario).to_JSON(), 200
+                return {"message": "Usuario no encontrado"}, 404
         except Exception as e:
             print(f"Error en get_usuario: {e}")
-            return {"error": "Error al obtener el usuario"}
+            return {"error": "Error al obtener el usuario"}, 500
         finally:
             release_connection(connection)
 
     @classmethod
     def get_usuario_by_email(cls, email):
+        """ Obtiene un usuario por su email. """
+        pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        if not re.match(pattern, email):
+            return {"error": "Correo electrónico inválido"}, 400
+
         try:
             connection = get_connection()
-
-            def is_valid_email(email):
-                pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
-                return re.match(pattern, email) is not None
-
-            if not is_valid_email(email):
-                return {"error": "Correo electrónico inválido"}  # Si el email no es válido
-            
             with connection.cursor() as cursor:
                 cursor.execute("SELECT id, username, email, password_hash FROM public.usuarios WHERE email=%s", (email,))
                 usuario = cursor.fetchone()
@@ -72,97 +68,43 @@ class UsuarioModel:
                         "username": usuario[1],
                         "email": usuario[2],
                         "password_hash": usuario[3]
-                    }
-                return None  # Si el usuario no existe
+                    }, 200
+                return {"message": "Usuario no encontrado"}, 404
         except Exception as e:
             print(f"Error en get_usuario_by_email: {e}")
-            return None
+            return {"error": "Error interno al obtener el usuario"}, 500
         finally:
             release_connection(connection)
 
-
-    
-
     @classmethod
     def add_usuario(cls, usuario):
+        """ Agrega un nuevo usuario a la base de datos. """
         try:
             connection = get_connection()
-
             with connection.cursor() as cursor:
                 password_hashed = hash_password(usuario.password_hash)
 
                 usuario.username = bleach.clean(usuario.username) if usuario.username else None
                 usuario.email = bleach.clean(usuario.email) if usuario.email else None
-                usuario.first_name = bleach.clean(usuario.first_name) if usuario.first_name else None
-                usuario.last_name = bleach.clean(usuario.last_name) if usuario.last_name else None
-                usuario.phone_number = bleach.clean(usuario.phone_number) if usuario.phone_number else None
-                usuario.address = bleach.clean(usuario.address) if usuario.address else None
-                usuario.city = bleach.clean(usuario.city) if usuario.city else None
-                usuario.country = bleach.clean(usuario.country) if usuario.country else None
 
-                birthdate = usuario.birthdate if usuario.birthdate else None
-                is_seller = usuario.is_seller if usuario.is_seller is not None else False
-                seller_rating = float(usuario.seller_rating) if usuario.seller_rating is not None else 0.0
-                cars_sold = usuario.cars_sold if usuario.cars_sold is not None else 0
-                registration_date = usuario.registration_date if usuario.registration_date else None
-                last_login = usuario.last_login if usuario.last_login else None
-                is_active = usuario.is_active if usuario.is_active is not None else True
-                is_admin = usuario.is_admin if usuario.is_admin is not None else False
+                cursor.execute("SELECT id FROM public.usuarios WHERE username = %s OR email = %s", (usuario.username, usuario.email))
+                existing_user = cursor.fetchone()
+                if existing_user:
+                    return {"error": "El usuario o el email ya están registrados"}, 400
 
-                cursor.execute("""INSERT INTO public.usuarios (username, email, password_hash, first_name, last_name, phone_number, address, city, country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin)
-                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                            (usuario.username, usuario.email, password_hashed, usuario.first_name, usuario.last_name, usuario.phone_number, usuario.address, usuario.city, usuario.country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin))
-                connection.commit()
+                cursor.execute("""
+                    INSERT INTO public.usuarios (username, email, password_hash, first_name, last_name, phone_number, address, city, country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                """, (usuario.username, usuario.email, password_hashed, usuario.first_name, usuario.last_name, usuario.phone_number, usuario.address, usuario.city, usuario.country, usuario.birthdate, usuario.is_seller, usuario.seller_rating, usuario.cars_sold, usuario.registration_date, usuario.last_login, usuario.is_active, usuario.is_admin))
 
-            connection.close()
-            return {"message": "Usuario agregado exitosamente"}
-
-        except UniqueViolation as e:
-            print(f"Error: {e}")
+                if cursor.rowcount == 1:
+                    connection.commit()
+                    return {"message": "Usuario agregado exitosamente"}, 201
+                return {"error": "No se pudo agregar el usuario"}, 500
+        except UniqueViolation:
             return {"error": "El usuario o el email ya están registrados"}, 400
-
         except Exception as e:
             print(f"Error en add_usuario: {e}")
-            return {"error": str(e)}, 500
-
-        finally:
-            release_connection(connection)
-
-
-    @classmethod
-    def delete_usuario(cls, id):
-        try:
-            connection = get_connection()
-
-            with connection.cursor() as cursor:
-                cursor.execute("DELETE FROM public.usuarios WHERE id = %s", (id,))
-                affected_rows = cursor.rowcount
-                connection.commit()
-
-            connection.close()
-            return affected_rows
-        except Exception as e:
-            print(f"Error en delete_usuario: {e}")
-            return {"error": "Error al eliminar el usuario"}
-        finally:
-            release_connection(connection)
-
-    @classmethod
-    def update_usuario(cls, usuario):
-        try:
-            connection = get_connection()
-
-            with connection.cursor() as cursor:
-                cursor.execute("""UPDATE public.usuarios SET username=%s, email=%s, password_hash=%s, first_name=%s, last_name=%s, phone_number=%s, address=%s, city=%s, country=%s, birthdate=%s, is_seller=%s, seller_rating=%s, cars_sold=%s, registration_date=%s, last_login=%s, is_active=%s, is_admin=%s
-                               WHERE id = %s""",
-                               (usuario.username, usuario.email, usuario.password_hash, usuario.first_name, usuario.last_name, usuario.phone_number, usuario.address, usuario.city, usuario.country, usuario.birthdate, usuario.is_seller, usuario.seller_rating, usuario.cars_sold, usuario.registration_date, usuario.last_login, usuario.is_active, usuario.is_admin, usuario.id))
-                affected_rows = cursor.rowcount
-                connection.commit()
-
-            connection.close()
-            return affected_rows
-        except Exception as e:
-            print(f"Error en update_usuario: {e}")
-            return {"error": "Error al actualizar el usuario"}
+            return {"error": f"Error interno: {str(e)}"}, 500
         finally:
             release_connection(connection)
--- backend/src/routes/Usuario.py
+++ backend/src/routes/Usuario.py
@@ -10,124 +10,32 @@ usuario_bp = Blueprint('usuario_bp', __name__)
 @usuario_bp.route('/login', methods=['POST'])
 @limiter.limit("3 per minute")
 def login():
-    try:
-        data = request.json
-        email = data.get("email")
-        password = data.get("password")
+    data = request.json
+    email = data.get("email")
+    password = data.get("password")
 
-        # Buscar usuario en la BD
-        usuario = UsuarioModel.get_usuario_by_email(email)
-        if not usuario:
-            return jsonify({"error": "Usuario no encontrado"}), 404
+    usuario, status_code = UsuarioModel.get_usuario_by_email(email)
+    if "error" in usuario or not usuario:
+        return jsonify(usuario), status_code
 
-        # Verificar contraseña
-        if not check_password(password, usuario["password_hash"]):
-            return jsonify({"error": "Contraseña incorrecta"}), 401
+    if not check_password(password, usuario["password_hash"]):
+        return jsonify({"error": "Contraseña incorrecta"}), 401
 
-        # Generar Token JWT
-        token = generate_jwt(usuario["id"])
-        return jsonify({"token": token}), 200
+    token = generate_jwt(usuario["id"])
+    return jsonify({"token": token}), 200
 
-    except Exception as e:
-        print(f"Error en login: {e}")
-        return jsonify({"error": "Error en el login"}), 500
-        
-        
-
-
-@usuario_bp.route('/', methods=['GET'])
-@require_jwt
-def get_usuarios():
-    try:
-        usuarios = UsuarioModel.get_usuarios()
-        return jsonify(usuarios), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-@usuario_bp.route('/<id>', methods=['GET'])
+@usuario_bp.route('/<int:id>', methods=['GET'])
 @require_jwt
 def get_usuario(id):
-    try:
-        usuario = UsuarioModel.get_usuario(id)
-        if usuario:
-            return jsonify(usuario), 200
-        return jsonify({'message': 'Usuario no encontrado'}), 404
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
+    return jsonify(UsuarioModel.get_usuario(id))
 
 @usuario_bp.route('/add', methods=['POST'])
 def add_usuario():
-    try:
-        data = request.json
-        nuevo_usuario = Usuario(
-            username=data['username'],
-            email=data['email'],
-            password_hash=data['password_hash'],
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            phone_number=data['phone_number'],
-            address=data['address'],
-            city=data['city'],
-            country=data['country'],
-            birthdate=data.get('birthdate'),
-            is_seller=data.get('is_seller', False),
-            seller_rating=data.get('seller_rating', 0.0),
-            cars_sold=data.get('cars_sold', 0),
-            registration_date=data.get('registration_date'),
-            last_login=data.get('last_login'),
-            is_active=data.get('is_active', True),
-            is_admin=data.get('is_admin', False)
-        )
-        affected_rows = UsuarioModel.add_usuario(nuevo_usuario)
-        if affected_rows == 1:
-            return jsonify({'message': 'Usuario agregado exitosamente'}), 201
-        return jsonify({'message': 'Error al agregar el usuario'}), 500
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    data = request.json
+    nuevo_usuario = Usuario(**data)
+    return jsonify(UsuarioModel.add_usuario(nuevo_usuario))
 
-
-@usuario_bp.route('/update/<id>', methods=['PUT'])
-@require_jwt
-def update_usuario(id):
-    try:
-        data = request.json
-        usuario_actualizado = Usuario(
-            id=id,
-            username=data['username'],
-            email=data['email'],
-            password_hash=data['password_hash'],
-            first_name=data['first_name'],
-            last_name=data['last_name'],
-            phone_number=data['phone_number'],
-            address=data['address'],
-            city=data['city'],
-            country=data['country'],
-            birthdate=data.get('birthdate'),
-            is_seller=data.get('is_seller', False),
-            seller_rating=data.get('seller_rating', 0.0),
-            cars_sold=data.get('cars_sold', 0),
-            registration_date=data.get('registration_date'),
-            last_login=data.get('last_login'),
-            is_active=data.get('is_active', True),
-            is_admin=data.get('is_admin', False)
-        )
-        affected_rows = UsuarioModel.update_usuario(usuario_actualizado)
-        if affected_rows == 1:
-            return jsonify({'message': 'Usuario actualizado exitosamente'}), 200
-        return jsonify({'message': 'Error al actualizar el usuario'}), 500
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-
-@usuario_bp.route('/delete/<id>', methods=['DELETE'])
+@usuario_bp.route('/delete/<int:id>', methods=['DELETE'])
 @require_jwt
 def delete_usuario(id):
-    try:
-        affected_rows = UsuarioModel.delete_usuario(id)
-        if affected_rows == 1:
-            return jsonify({'message': 'Usuario eliminado exitosamente'}), 200
-        return jsonify({'message': 'Error al eliminar el usuario'}), 500
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    return jsonify(UsuarioModel.delete_usuario(id))
