2e204f3c01ae517d6f7499dfd9fe995c3e255c52
Germán Rojas <garcejr@gmail.com>
2025-02-18 22:14:35 -0500

    Refactor add_usuario method to handle optional fields and improve error handling for unique constraint violations

--- backend/src/models/UsuarioModel.py
+++ backend/src/models/UsuarioModel.py
@@ -3,7 +3,7 @@ from .entities.Usuario import Usuario
 from utils.security import hash_password
 import re
 import bleach
-
+from psycopg2.errors import UniqueViolation
 
 class UsuarioModel:
 
@@ -81,36 +81,53 @@ class UsuarioModel:
             release_connection(connection)
 
 
+    
+
     @classmethod
     def add_usuario(cls, usuario):
         try:
             connection = get_connection()
 
             with connection.cursor() as cursor:
-                password_hashed = hash_password(usuario.password_hash) # Ciframos la contraseña
-                
-                # Limpiar los campos de texto
-                usuario.username = bleach.clean(usuario.username)
-                usuario.email = bleach.clean(usuario.email)
-                usuario.first_name = bleach.clean(usuario.first_name)
-                usuario.last_name = bleach.clean(usuario.last_name)
-                usuario.phone_number = bleach.clean(usuario.phone_number)
-                usuario.address = bleach.clean(usuario.address)
-                usuario.city = bleach.clean(usuario.city)
-                usuario.country = bleach.clean(usuario.country)
-                
+                password_hashed = hash_password(usuario.password_hash)
+
+                usuario.username = bleach.clean(usuario.username) if usuario.username else None
+                usuario.email = bleach.clean(usuario.email) if usuario.email else None
+                usuario.first_name = bleach.clean(usuario.first_name) if usuario.first_name else None
+                usuario.last_name = bleach.clean(usuario.last_name) if usuario.last_name else None
+                usuario.phone_number = bleach.clean(usuario.phone_number) if usuario.phone_number else None
+                usuario.address = bleach.clean(usuario.address) if usuario.address else None
+                usuario.city = bleach.clean(usuario.city) if usuario.city else None
+                usuario.country = bleach.clean(usuario.country) if usuario.country else None
+
+                birthdate = usuario.birthdate if usuario.birthdate else None
+                is_seller = usuario.is_seller if usuario.is_seller is not None else False
+                seller_rating = float(usuario.seller_rating) if usuario.seller_rating is not None else 0.0
+                cars_sold = usuario.cars_sold if usuario.cars_sold is not None else 0
+                registration_date = usuario.registration_date if usuario.registration_date else None
+                last_login = usuario.last_login if usuario.last_login else None
+                is_active = usuario.is_active if usuario.is_active is not None else True
+                is_admin = usuario.is_admin if usuario.is_admin is not None else False
+
                 cursor.execute("""INSERT INTO public.usuarios (username, email, password_hash, first_name, last_name, phone_number, address, city, country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin)
-                               VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
-                               (usuario.username, usuario.email, password_hashed, usuario.first_name, usuario.last_name, usuario.phone_number, usuario.address, usuario.city, usuario.country, usuario.birthdate, usuario.is_seller, usuario.seller_rating, usuario.cars_sold, usuario.registration_date, usuario.last_login, usuario.is_active, usuario.is_admin))
+                            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                            (usuario.username, usuario.email, password_hashed, usuario.first_name, usuario.last_name, usuario.phone_number, usuario.address, usuario.city, usuario.country, birthdate, is_seller, seller_rating, cars_sold, registration_date, last_login, is_active, is_admin))
                 connection.commit()
 
             connection.close()
             return {"message": "Usuario agregado exitosamente"}
+
+        except UniqueViolation as e:
+            print(f"Error: {e}")
+            return {"error": "El usuario o el email ya están registrados"}, 400
+
         except Exception as e:
             print(f"Error en add_usuario: {e}")
-            return {"error": "Error al agregar el usuario", "message": str(e)}
+            return {"error": str(e)}, 500
+
         finally:
             release_connection(connection)
+
 
     @classmethod
     def delete_usuario(cls, id):
